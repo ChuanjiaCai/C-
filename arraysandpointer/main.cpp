43b61faf8280cc00badf50825d19a3a9e75e8c41
jack <jack@jackdemacbook-air.local>
2016-06-13 01:49:47 +0800

    Initial Commit

---
+++ arraysandpointer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  arraysandpointer
+//
+//  Created by jack on 16/6/13.
+//  Copyright © 2016年 jack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
