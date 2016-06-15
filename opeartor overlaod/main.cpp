6ee90bdbc63c40c27d51cf2fac2ffe4c8f86a0b3
jack <jack@jackdemacbook-air.local>
2016-06-15 16:44:26 +0800

    Initial Commit

---
+++ opeartor overlaod/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opeartor overlaod
+//
+//  Created by jack on 16/6/15.
+//  Copyright © 2016年 jack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
