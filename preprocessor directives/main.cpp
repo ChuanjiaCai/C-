839f46483cd184b0ab3d7aa87e0fd7b4346e7e5a
jack <jack@jackdemacbook-air.local>
2016-08-11 23:24:08 +0800

    Initial Commit

---
+++ preprocessor directives/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  preprocessor directives
+//
+//  Created by jack on 16/8/11.
+//  Copyright © 2016年 jack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
